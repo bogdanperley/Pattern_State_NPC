f659204d45c05c036d5cdeb07ae32a82410b2081
Богдан Перлей <bogdanperl2002@gmail.com>
2022-11-26 18:06:49 +0200

    Fix commints

--- NPC_Behavior.cpp
+++ NPC_Behavior.cpp
@@ -1,85 +1,100 @@
 #include "NPC_Behavior.h"
+NPC::NPC() : state_{ new On_guard(state_) } {}
 
-NPC::NPC() : state_{new On_guard(state_)} {}
+bool NPC::has_energy() { return energy_ > 50; }
 
-bool NPC::has_energy() { return this->energy_ > 50; }
-
-void NPC::call_energy_ok() { Fight::energy_Ok; }
 
 void NPC::On_guard() {
-  auto random{rand() % 5};
-  if (random < 2) {
-    state_->see_small_enemy();
-    return;
-  }
-  state_->see_big_enemy();
+    constexpr int see_small_enemy{ 2 };
+    constexpr int see_big_enemy{ 5 };
+    auto random{ rand() % 5 };
+    while (random == see_small_enemy || random == see_big_enemy)
+    {
+        if (random == see_small_enemy) {
+            state_->see_small_enemy();
+            return;
+        }
+        if (random == see_big_enemy) {
+            state_->see_big_enemy();
+            return;
+        }
+      std::cout << "NPC On guard!" << std::endl;
+    }
 }
 
+void NPC::call_energy_ok() { Fight(); } // TO DO
+
 void NPC::Fight() {
-  auto random{rand() % 2};
-  if (random == 2) {
-    std::cout << "NPC Wounded!" << std::endl;
-    this->energy_ -= 50;
-  } else {
-    std::cout << "NPC wounded the enemy!" << std::endl;
-  }
-  if (has_energy() == true) {
-    this->call_energy_ok();
-    return;
-  }
-  state_->losing_fight();
+    auto random{ rand() % 2 };
+    constexpr int NPC_Wounded{ 2 };
+    if (random == NPC_Wounded) {
+        std::cout << "NPC Wounded!" << std::endl;
+        energy_ -= 50;
+    }
+    else {
+        std::cout << "NPC wounded the enemy!" << std::endl;
+    }
+    if (has_energy()) {
+        this->call_energy_ok();
+        return;
+    }
+    state_->losing_fight();
 }
 
 void NPC::Run_away() {
-  if (has_energy() == true) {
-    auto random{rand() % 3};
-    if (random == 2) {
-      std::cout << "NPC could not escape from a big enemy, NPC DIED!"
+    if (has_energy()) {
+        auto random{ rand() % 3 };
+        constexpr int NPC_DIED{ 2 };
+        if (random == NPC_DIED) {
+            std::cout << "NPC could not escape from a big enemy, NPC DIED!"
                 << std::endl;
-      state_ = nullptr;
-      return;
-    } else {
-      std::cout << " Ran away from the big enemy " << std::endl;
-      state_->escaped();
+            delete state_;
+            return;
+        }
+        else {
+            std::cout << " Ran away from the big enemy " << std::endl;
+            state_->escaped();
+        }
     }
-  } else {
-    auto random{rand() % 2};
-    if (random == 1) {
-      std::cout << "It was not possible to restore energy, NPC DIED "
+    else {
+        auto random{ rand() % 2 };
+        constexpr int not_possible_to_restore_energy{ 1 };
+        if (random == not_possible_to_restore_energy) {
+            std::cout << "It was not possible to restore energy, NPC DIED "
                 << std::endl;
-      state_ = nullptr;
-    } else {
-      std::cout << "Energy recovered " << std::endl;
-      energy_ += 50;
-      state_->escaped();
+            delete state_;
+        }
+        else {
+            std::cout << "Energy recovered " << std::endl;
+            energy_ += 50;
+            state_->escaped();
+        }
     }
-  }
 }
 
 void On_guard::see_small_enemy() {
-  NPC_->change_state(new Fight(NPC_));
-  NPC_->Fight();
+    NPC_->change_state(new Fight(NPC_));
+    NPC_->Fight();
 }
 
 void On_guard::see_big_enemy() {
-  NPC_->change_state(new Run_away(NPC_));
-  NPC_->Run_away();
+    NPC_->change_state(new Run_away(NPC_));
+    NPC_->Run_away();
 }
 
 void Fight::losing_fight() {
-  NPC_->change_state(new Run_away(NPC_));
-  NPC_->Run_away();
+    NPC_->change_state(new Run_away(NPC_));
+    NPC_->Run_away();
 }
 
-void Fight::energy_Ok() { NPC_->Fight(); }
+void Fight::energy_Ok() { NPC_->change_state( new Fight(NPC_)); }
 
 void Run_away::escaped() {
-  NPC_->change_state(new On_guard(NPC_));
-  NPC_->On_guard();
+    NPC_->change_state(new On_guard(NPC_));
+    NPC_->On_guard();
 }
 
 void Run_away::died() {
-  NPC_->change_state(new On_guard(NPC_));
-  NPC_->On_guard();
+    NPC_->change_state(new On_guard(NPC_));
+    NPC_->On_guard();
 }
-
--- NPC_Behavior.h
+++ NPC_Behavior.h
@@ -4,51 +4,52 @@
 
 class NPC {
 public:
-  NPC();
-  void change_state(State *state) {
-    delete state_;
-    state_ = state;
-  };
-  void On_guard();
-  void Run_away();
-  void Fight();
-  bool has_energy();
-  void call_energy_ok();
+    NPC();
+    void change_state(State* state) {
+        delete state_;
+        state_ = state;
+    };
+    void On_guard();
+    void Run_away();
+    void Fight();
+    bool has_energy();
+    void call_energy_ok();
 
 private:
-  State *state_{nullptr};
-  int energy_{100};
+    State* state_{ nullptr };
+    int energy_{ 100 };
 };
 class State {
 public:
-  State(NPC *npc) : NPC_{npc} {}
-  virtual void see_small_enemy() { std::cout << "NOT IMPLEMENTED!" << std::endl;}
-  virtual void see_big_enemy() { std::cout << "NOT IMPLEMENTED!" << std::endl; }
-  virtual void losing_fight() { std::cout << "NOT IMPLEMENTED!" << std::endl; }
-  virtual void escaped() { std::cout << "NOT IMPLEMENTED!" << std::endl; }
-  virtual void set_Npc() { std::cout << "NOT IMPLEMENTED!" << std::endl; }
+    State(NPC* npc) : NPC_{ npc } {}
+    virtual void see_small_enemy() { std::cout << "NOT IMPLEMENTED!" << std::endl;}
+    virtual void see_big_enemy() { std::cout << "NOT IMPLEMENTED!" << std::endl; }
+    virtual void losing_fight() { std::cout << "NOT IMPLEMENTED!" << std::endl; }
+    virtual void escaped() { std::cout << "NOT IMPLEMENTED!" << std::endl; }
+    virtual void set_Npc(State* state) { std::cout << "NOT IMPLEMENTED!" << std::endl; }
+    virtual void died() { std::cout << "NOT IMPLEMENTED!" << std::endl; };
 
 protected:
-  NPC *NPC_{nullptr};
+    NPC* NPC_{ nullptr };
 };
 
 class On_guard : public State {
 public:
-  using State::State; // On_guard(NPC* npc):State(npc){ }
-  void see_small_enemy() override;
-  void see_big_enemy() override;
+    using State::State; // On_guard(NPC* npc):State(npc){ }
+    void see_small_enemy() override;
+    void see_big_enemy() override;
 };
 
 class Fight : public State {
 public:
-  using State::State; // Fight(NPC* npc) :State(npc) { }
-  void losing_fight() override;
-  void energy_Ok();
+    using State::State; // Fight(NPC* npc) :State(npc) { }
+    void losing_fight() override;
+    void energy_Ok();
 };
 
 class Run_away : public State {
 public:
-  using State::State; // Run_away(NPC* npc) :State(npc) { }
-  void escaped() override;
-  void died();
+    using State::State; // Run_away(NPC* npc) :State(npc) { }
+    void escaped() override;
+    void died()override;
 };
